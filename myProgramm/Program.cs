47949b63e3ca4def7d51eadac136b6f31ea6e78f
PonomarevSN <s-ponomarev@mail.ru>
2022-11-30 00:45:54 +0300

    myProgramm/ForMainMetod.jpg

---
+++ myProgramm/Program.cs
@@ -0,0 +1,29 @@
+
+
+void NewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("Введите текст: ");
+string[] array = Console.ReadLine().Split();
+string[] array2 = new string[array.Length];
+NewArray(array, array2);
+Console.WriteLine();
+Console.WriteLine("массив из строк длинна которых меньше либо равна 3 символа: ");
+PrintArray(array2);
